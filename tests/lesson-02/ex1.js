6084a49ceb293bbd2928f60f521b5c23d43dfeeb
nguyenthidiem-qc <nguyenthidiem056@gmail.com>
2026-09-19 15:35:09 +0700

    ex1-2-3

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,27 @@
+//a. Khai báo hằng số number có giá trị 12
+const number = 12;
+console.log(number);
+
+//b. Khai báo biến name với giá trị là my number
+let name = "my number";
+console.log(name);
+
+//note: var và let (var xài cho global, let phạm vị 1 block code {})
+{
+    var a = 10;
+    let b = 11;
+    console.log(b);
+}
+console.log(a);
+
+//c. Khai báo 1 biến isEvent với giá trị sai
+let  isEvent = false;
+console.log(isEvent);
+
+//d. Kiểm tra nếu number là số chẵn gán isEvent thành giá trị đúng
+let number1 = 20;
+if (number1 % 2 === 0) {
+    isEvent = true;
+    console.log(isEvent);
+
+}
