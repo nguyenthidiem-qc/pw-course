e0d02ef908b6dffce4613d3dec6899967fa861fb
nguyenthidiem-qc <nguyenthidiem056@gmail.com>
2026-09-21 02:11:42 +0700

    complete all my exs

---
+++ tests/lesson-02/ex9.js
@@ -0,0 +1,16 @@
+//Tạo file ex9.js, thêm vào code đáp án cho đề bài sau:
+//a. In ra các giá trị từ 25 về 12
+
+for(i = 25; i >= 12; i--){
+    console.log(i)
+}
+
+/* Kết quả:
+25
+24
+23
+22
+21
+...
+12
+*/
