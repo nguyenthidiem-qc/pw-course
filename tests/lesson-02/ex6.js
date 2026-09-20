003c272f6517491fda415948598f974a4ef303e1
nguyenthidiem-qc <nguyenthidiem056@gmail.com>
2026-09-21 01:48:41 +0700

    add ex6-7

---
+++ tests/lesson-02/ex6.js
@@ -0,0 +1,27 @@
+//Tạo file ex6.js, thêm vào code đáp án cho đề bài sau:
+//a. In ra giá trị từ 12 đến 30
+for (i = 12; i <= 30; i++) {
+    console.log(i);
+}
+
+/*Kết quả:
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+*/
---
+++ tests/lesson-02/ex7.js
@@ -0,0 +1,19 @@
+//Tạo file ex7.js, thêm vào code đáp án cho đề bài sau:
+//a. In ra các giá trị chia hết cho 3 từ 1000 đến 2000
+
+for (i = 1000; i <= 2000; i++) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+}
+
+/*Kết quả
+1002
+1005
+1008
+1011
+1014
+1017
+...
+1998
+*/
